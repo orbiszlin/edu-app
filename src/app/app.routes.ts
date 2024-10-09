13d27d4e32b24190e1217051608c7ea10fa30055
feltwine <patik.vaclav@proton.me>
2024-10-09 12:37:58 +0200

    Adjusted app.routes.ts with added path for pages/game

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,9 @@ export const routes: Routes = [
     path: 'folder/:id',
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
+  },  {
+    path: 'game',
+    loadComponent: () => import('./pages/game/game.page').then( m => m.GamePage)
   },
+
 ];
