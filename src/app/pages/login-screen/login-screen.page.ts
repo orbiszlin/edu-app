c9b1df893b9d269eb81746ad803d13dd281fae29
Zu <zupapa@email.cz>
2024-10-16 12:30:25 +0200

    Added buttons (not functional for now

--- src/app/pages/login-screen/login-screen.page.ts
+++ src/app/pages/login-screen/login-screen.page.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonRow,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.page.html',
   styleUrls: ['./login-screen.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonGrid, IonCol, IonRow]
 })
 export class LoginScreenPage implements OnInit {
 
@@ -18,3 +27,4 @@ export class LoginScreenPage implements OnInit {
   }
 
 }
+
