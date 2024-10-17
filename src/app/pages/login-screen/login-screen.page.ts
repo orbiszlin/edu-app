f791fb3cb639f5a9ce9615fb60eb483650d220ab
Zu <zupapa@email.cz>
2024-10-17 15:04:15 +0200

    buttons now do an alert

--- src/app/pages/login-screen/login-screen.page.ts
+++ src/app/pages/login-screen/login-screen.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
+  IonAlert,
   IonButton,
   IonCol,
   IonContent,
@@ -17,9 +18,10 @@ import {
   templateUrl: './login-screen.page.html',
   styleUrls: ['./login-screen.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonGrid, IonCol, IonRow]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonGrid, IonCol, IonRow, IonAlert]
 })
 export class LoginScreenPage implements OnInit {
+  alertButtons = ['Action'];
 
   constructor() { }
 
