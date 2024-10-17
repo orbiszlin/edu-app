72576d8d7691e1a0eaa55658dc8c35dd7920afd7
JackKybl <jakub.zamecnik@orbiszlin.cz>
2024-10-17 15:35:15 +0200

    - Added button - Added Input field - Added CSS

--- src/app/pages/avatar-settings/avatar-settings.page.ts
+++ src/app/pages/avatar-settings/avatar-settings.page.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonButtons, IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonButtons, IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader, IonInput,
+  IonMenuButton, IonRow,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-avatar-settings',
   templateUrl: './avatar-settings.page.html',
   styleUrls: ['./avatar-settings.page.scss'],
   standalone: true,
-    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonGrid, IonCol, IonRow, IonButton, IonInput]
 })
 export class AvatarSettingsPage implements OnInit {
 
