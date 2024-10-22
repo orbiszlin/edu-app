d6379c1057301d5ba27e7c841a812c66dd27f3e9
feltwine <patik.vaclav@proton.me>
2024-10-22 10:37:49 +0200

    Implementation of map data service into game page

--- src/app/pages/game/game.page.ts
+++ src/app/pages/game/game.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {MapData, MapDataService} from 'src/app/services/map-data/map-data.service'
 
 @Component({
   selector: 'app-game',
@@ -10,11 +11,25 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class GamePage implements OnInit {
+export class GamePage implements OnInit, AfterViewInit {
+  baseHex: string = "assets/map-tiles/map-tile-hex.svg";
+  @ViewChild('canvas', { static: false }) canvasRef!: ElementRef<HTMLCanvasElement>;
+  mapData!: MapData;
 
-  constructor() { }
+  constructor(private mapDataService: MapDataService) { }
 
   ngOnInit() {
+    this.mapDataService.getMapData().subscribe(data => {
+      this.mapData = data;
+      console.log(this.mapData);
+    })
   }
 
+  ngAfterViewInit() {
+    this.drawSvgOnCanvas();
+  }
+
+  drawSvgOnCanvas() {
+    console.log(this.canvasRef)
+  }
 }
