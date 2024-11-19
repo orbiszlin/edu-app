4909df9740b4cb4ade3ce05a04fdcf37308e0dfb
zdenek.kolar <zdenek05kolar@seznam.cz>
2024-11-19 10:45:37 +0100

    Final version

--- src/app/pages/answers/answers.page.ts
+++ src/app/pages/answers/answers.page.ts
@@ -1,25 +1,48 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {AnswService} from "../../services/answ.service";
+import { AnswService } from '../../services/answ.service';
 import {
-  IonButton, IonCol,
-  IonContent, IonGrid,
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
   IonHeader,
   IonItem,
-  IonLabel, IonList,
-  IonRadio, IonRadioGroup, IonRow, IonText,
+  IonLabel,
+  IonList,
+  IonRadio,
+  IonRadioGroup,
+  IonRow,
+  IonText,
   IonTitle,
   IonToolbar
 } from '@ionic/angular/standalone';
-import {Question} from "../../models/question.model";
+import { Question } from '../../models/question.model';
 
 @Component({
   selector: 'app-answers',
   templateUrl: './answers.page.html',
   styleUrls: ['./answers.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonRadio, IonItem, IonLabel, IonRadioGroup, IonList, IonGrid, IonRow, IonCol, IonText]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButton,
+    IonRadio,
+    IonItem,
+    IonLabel,
+    IonRadioGroup,
+    IonList,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonText
+  ]
 })
 export class AnswersPage implements OnInit {
   questions: Question[] = [];
@@ -28,24 +51,35 @@ export class AnswersPage implements OnInit {
   selectedAnswer: string = '';
   isAnswerCorrect: boolean | null = null;
   answerFeedback: string | null = null;
-  remainingTime: string = ''; // Nastavte čas, pokud je to potřeba
+  remainingTime: string = ''; // Lze využít pro časový limit
 
-  constructor(
-    private answService: AnswService,
-  ) {}
+  constructor(private answService: AnswService) {}
 
   ngOnInit() {
+    this.initializeQuiz();
+  }
+
+  /**
+   * Inicializace kvízu: načtení otázek a příprava první otázky.
+   */
+  initializeQuiz() {
     this.questions = this.answService.getQuestions();
     this.totalQuestions = this.questions.length;
     this.loadQuestion();
   }
 
+  /**
+   * Načtení aktuální otázky a reset výchozích hodnot pro odpověď.
+   */
   loadQuestion() {
     this.selectedAnswer = '';
     this.isAnswerCorrect = null;
     this.answerFeedback = null;
   }
 
+  /**
+   * Odeslání odpovědi, vyhodnocení a zobrazení zpětné vazby.
+   */
   submitAnswer() {
     const currentQuestion = this.questions[this.currentQuestionIndex];
     const correctAnswer = currentQuestion.correctAnswer;
@@ -58,35 +92,53 @@ export class AnswersPage implements OnInit {
 
     setTimeout(() => {
       this.moveToNextQuestion();
-    }, 1500); // Čas na zobrazení zpětné vazby
+    }, 1500); // Zpoždění pro zobrazení zpětné vazby
   }
 
+  /**
+   * Posun na další otázku nebo ukončení kvízu, pokud byl poslední dotaz.
+   */
   moveToNextQuestion() {
     if (this.currentQuestionIndex < this.totalQuestions - 1) {
       this.currentQuestionIndex++;
       this.loadQuestion();
     } else {
-      console.log('Konec kvízu.');
-      // Můžete zde přidat logiku pro ukončení kvízu
+      this.endQuiz();
     }
   }
 
-  getAnswerClass(answer: string) {
+  /**
+   * Vrátí CSS třídu pro odpověď na základě její správnosti.
+   * @param answer Odpověď, která se vyhodnocuje
+   */
+  getAnswerClass(answer: string): string {
     if (this.isAnswerCorrect === null) {
-      return '';
+      return ''; // Odpověď zatím nebyla vyhodnocena
     }
     if (this.isAnswerCorrect && answer === this.questions[this.currentQuestionIndex].correctAnswer) {
-      return 'correct-answer';
-    } else if (!this.isAnswerCorrect && answer === this.selectedAnswer) {
-      return 'incorrect-answer';
+      return 'correct-answer'; // Správná odpověď
     }
-    return '';
+    if (!this.isAnswerCorrect && answer === this.selectedAnswer) {
+      return 'incorrect-answer'; // Špatná odpověď
+    }
+    return ''; // Žádná speciální třída
   }
 
-  getCorrectAnswerLabel() {
-    const correctOption = this.questions[this.currentQuestionIndex].options.find(
+  /**
+   * Vrátí popisek správné odpovědi.
+   */
+  getCorrectAnswerLabel(): string {
+    const correctOption = this.questions[this.currentQuestionIndex]?.options.find(
       option => option.value === this.questions[this.currentQuestionIndex].correctAnswer
     );
     return correctOption ? correctOption.label : '';
+  }
+
+  /**
+   * Akce při ukončení kvízu (lze rozšířit o logiku pro zobrazení výsledků).
+   */
+  endQuiz() {
+    console.log('Konec kvízu.');
+    // Místo pro logiku při ukončení kvízu, např. navigace na jinou stránku
   }
 }
